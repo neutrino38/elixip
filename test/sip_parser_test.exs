8d7849c3c142e5e8c612197f933d36d1d3063d56
Emmanuel Buu <emmanuel.buu@ives.fr>
2023-10-19 09:11:55 +0200

    ajout test unitaire sur la serialisation des messages SIP

--- test/sip_parser_test.exs
+++ test/sip_parser_test.exs
@@ -1,16 +1,16 @@
 defmodule SIPParserTest do
   use ExUnit.Case
-  doctest SIPUriParser
+  doctest SIPUri
 
   test "Parse an URI that has only a domain" do
-		{ code, parsed_uri } = SIPUriParser.parse_sip_uri("sip:domain.fr")
+		{ code, parsed_uri } = SIPUri.parse("sip:domain.fr")
 		assert code == :ok
 		assert parsed_uri.domain == "domain.fr"
   end
 
   test "Parse a simple URI" do
 
-		{ code, parsed_uri } = SIPUriParser.parse_sip_uri("sip:simple@domain.fr")
+		{ code, parsed_uri } = SIPUri.parse("sip:simple@domain.fr")
 		assert code == :ok
 		assert parsed_uri.userpart == "simple"
 		assert parsed_uri.domain == "domain.fr"
@@ -19,12 +19,12 @@ defmodule SIPParserTest do
   end
 
   test "Parse a faulty SIP URI" do
-		{ code, _parsed_uri } = SIPUriParser.parse_sip_uri("sipp:simple@domain.fr")
+		{ code, _parsed_uri } = SIPUri.parse("sipp:simple@domain.fr")
 		assert code == :invalid_sip_uri_general
 	end
 
   test "Parse an URI with transport parameter" do
-		{ code, parsed_uri } = SIPUriParser.parse_sip_uri("sip:simple@domain.fr:5060;transport=TCP")
+		{ code, parsed_uri } = SIPUri.parse("sip:simple@domain.fr:5060;transport=TCP")
 			assert code == :ok
 			assert parsed_uri.userpart == "simple"
 			assert parsed_uri.domain == "domain.fr"
@@ -33,7 +33,7 @@ defmodule SIPParserTest do
 	end
 
   test "Parse an URI with <> and transport parameter" do
-			{ code, parsed_uri } = SIPUriParser.parse_sip_uri("<sip:simple@domain.fr:5030>;transport=TCP;rport")
+			{ code, parsed_uri } = SIPUri.parse("<sip:simple@domain.fr:5030>;transport=TCP;rport")
 			assert code == :ok
 			assert parsed_uri.userpart == "simple"
 			assert parsed_uri.domain == "domain.fr"
@@ -42,7 +42,7 @@ defmodule SIPParserTest do
 	end
 
   test "Parse an URI with a display name and several parameters" do
-			{ code, parsed_uri } = SIPUriParser.parse_sip_uri("\"omé tür\" <sip:simple@domain.fr:50>;transport=TCP;rport")
+			{ code, parsed_uri } = SIPUri.parse("\"omé tür\" <sip:simple@domain.fr:50>;transport=TCP;rport")
 			assert code == :ok
 			assert parsed_uri.userpart == "simple"
 			assert parsed_uri.domain == "domain.fr"
@@ -52,16 +52,30 @@ defmodule SIPParserTest do
 	end
 
 	test "Parse an URI with a display name without spece" do
-		{ code, parsed_uri } = SIPUriParser.parse_sip_uri("\"Site%20Arras%20POLE%20EMPLOI\"<sip:+33970260233@visioassistance.net>;tag=8075639")
+		{ code, parsed_uri } = SIPUri.parse("\"Site%20Arras%20POLE%20EMPLOI\"<sip:+33970260233@visioassistance.net>;tag=8075639")
 		assert code == :ok
 		assert parsed_uri.params == %{ "tag" => "8075639" }
 	end
 
+	test "Serialize a SIP URI" do
+		uri = %{
+			port: 50,
+			scheme: "sip:",
+			domain: "domain.fr",
+			params: %{"rport" => true, "transport" => "TCP"},
+			userpart: "simple",
+			displayname: "omé tür"
+		}
+
+		{ code, uristr } = SIPUri.serialize(uri)
+		assert code == :ok
+		assert uristr == "\"om%C3%A9+t%C3%BCr\" <sip:simple@domain.fr:50>;rport;transport=TCP"
+	end
   test "Load and parse a REGISTER message" do
     { code, msg } = File.read("test/SIP-REGISTER.txt")
 		assert code == :ok
 
-		{ code, parsed_msg } = SIPMsgParser.parse(msg, fn code, errmsg, lineno, line ->
+		{ code, parsed_msg } = SIPMsg.parse(msg, fn code, errmsg, lineno, line ->
 			IO.puts("\n" <> errmsg)
 			IO.puts("Offending line #{lineno}: #{line}")
 			IO.puts("Error code #{code}")
@@ -80,7 +94,7 @@ defmodule SIPParserTest do
     { code, msg } = File.read("test/SIP-REGISTER-LVP.txt")
 		assert code == :ok
 
-		{ code, parsed_msg } = SIPMsgParser.parse(msg, fn code, errmsg, lineno, line ->
+		{ code, parsed_msg } = SIPMsg.parse(msg, fn code, errmsg, lineno, line ->
 			IO.puts("\n" <> errmsg)
 			IO.puts("Offending line #{lineno}: #{line}")
 			IO.puts("Error code #{code}")
@@ -96,7 +110,7 @@ defmodule SIPParserTest do
 		{ code, msg } = File.read("test/SIP-unREGISTER.txt")
 		assert code == :ok
 
-		{ code, _parsed_msg } = SIPMsgParser.parse(msg, fn _code, _errmsg, lineno, line ->
+		{ code, _parsed_msg } = SIPMsg.parse(msg, fn _code, _errmsg, lineno, line ->
 			assert lineno == 1
 			assert line == "UNREGISTER sip:example.com SIP/2.0"
 			end)
@@ -107,7 +121,7 @@ defmodule SIPParserTest do
 		{ code, data } = File.read("test/SDP-SIMPLE.txt")
 		assert code == :ok
 
-		bodylist = SIPMsgParser.parse_multi_part_body("application/sdp", data)
+		bodylist = SIPMsg.parse_multi_part_body("application/sdp", data)
 		assert code == :ok
 		[ body ] = bodylist
 		assert body.contenttype == "application/sdp"
@@ -119,7 +133,7 @@ defmodule SIPParserTest do
   	{ code, msg } = File.read("test/SIP-INVITE-BASIC-AUDIO.txt")
 		assert code == :ok # Test if file containing the SIP message is loaded
 
-		{ code, parsed_msg } = SIPMsgParser.parse(msg, fn code, errmsg, lineno, line ->
+		{ code, parsed_msg } = SIPMsg.parse(msg, fn code, errmsg, lineno, line ->
 			IO.puts("\n" <> errmsg)
 			IO.puts("Offending line #{lineno}: #{line}")
 			IO.puts("Error code #{code}")
@@ -140,7 +154,7 @@ defmodule SIPParserTest do
 	{ code, msg } = File.read("test/SIP-INVITE-LOST.txt")
   	assert code == :ok # Test if file containing the SIP message is loaded
 
-  	{ code, parsed_msg } = SIPMsgParser.parse(msg, fn code, errmsg, lineno, line ->
+  	{ code, parsed_msg } = SIPMsg.parse(msg, fn code, errmsg, lineno, line ->
 	  IO.puts("\n" <> errmsg)
 	  IO.puts("Offending line #{lineno}: #{line}")
 	  IO.puts("Error code #{code}")
@@ -158,7 +172,7 @@ defmodule SIPParserTest do
 			{ code, msg } = File.read("test/SIP-INVITE-LVP.txt")
 			assert code == :ok # Test if file containing the SIP message is loaded
 
-			{ code, parsed_msg } = SIPMsgParser.parse(msg, fn code, errmsg, lineno, line ->
+			{ code, parsed_msg } = SIPMsg.parse(msg, fn code, errmsg, lineno, line ->
 			IO.puts("\n" <> errmsg)
 			IO.puts("Offending line #{lineno}: #{line}")
 			IO.puts("Error code #{code}")
@@ -175,7 +189,7 @@ defmodule SIPParserTest do
 			{ code, msg } = File.read("test/SIP-180-LVP.txt")
 			assert code == :ok # Test if file containing the SIP message is loaded
 
-			{ code, parsed_msg } = SIPMsgParser.parse(msg, fn code, errmsg, lineno, line ->
+			{ code, parsed_msg } = SIPMsg.parse(msg, fn code, errmsg, lineno, line ->
 			IO.puts("\n" <> errmsg)
 			IO.puts("Offending line #{lineno}: #{line}")
 			IO.puts("Error code #{code}")
@@ -191,7 +205,7 @@ defmodule SIPParserTest do
 			{ code, msg } = File.read("test/SIP-200-LVP.txt")
 			assert code == :ok # Test if file containing the SIP message is loaded
 
-			{ code, parsed_msg } = SIPMsgParser.parse(msg, fn code, errmsg, lineno, line ->
+			{ code, parsed_msg } = SIPMsg.parse(msg, fn code, errmsg, lineno, line ->
 				IO.puts("\n" <> errmsg)
 				IO.puts("Offending line #{lineno}: #{line}")
 				IO.puts("Error code #{code}")
@@ -204,4 +218,31 @@ defmodule SIPParserTest do
 			assert parsed_msg.dialog_id == {"8075639", "32645600-4c01-bc8f-670c-deac31158db8", "as424e7930"}
 		end
 
+		test "Parse an BYE then reserialize it then reparse it" do
+			{ code, msg } = File.read("test/SIP-BYE-LVP.txt")
+			assert code == :ok # Test if file containing the SIP message is loaded
+
+			{ code, parsed_msg } = SIPMsg.parse(msg, fn code, errmsg, lineno, line ->
+				IO.puts("\n" <> errmsg)
+				IO.puts("Offending line #{lineno}: #{line}")
+				IO.puts("Error code #{code}")
+			end)
+
+			assert code == :ok
+			assert parsed_msg.method == :BYE
+			assert parsed_msg.dialog_id == {"8075639", "32645600-4c01-bc8f-670c-deac31158db8", "as424e7930"}
+
+			msg2 = SIPMsg.serialize(parsed_msg)
+			# IO.puts("\n")
+			# IO.puts(msg2)
+			{ code, parsed_msg2 } = SIPMsg.parse(msg2, fn code, errmsg, lineno, line ->
+				IO.puts("\n" <> errmsg)
+				IO.puts("Offending line #{lineno}: #{line}")
+				IO.puts("Error code #{code}")
+			end)
+
+			assert code == :ok
+			assert parsed_msg2.method == :BYE
+			assert parsed_msg2.dialog_id == {"8075639", "32645600-4c01-bc8f-670c-deac31158db8", "as424e7930"}
+		end
 end
