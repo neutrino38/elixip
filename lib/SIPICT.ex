c6ff708e826af8399fc1906377c38ca7d436b51f
Emmanuel Buu <emmanuel.buu@ives.fr>
2023-10-28 21:53:04 +0200

    Added ICT transctions Created an specific module for SIP timers handling

---
+++ lib/SIPICT.ex
@@ -0,0 +1,213 @@
+defmodule SIP.ICT do
+  @moduledoc "SIP INVITE client transaction"
+  use GenServer
+  import SIP.Trans.Timer
+  require SIPMsgOps
+  require Logger
+
+  defp safe_serialize(initial_state, sipmsg) do
+    try do
+      msgstr = SIPMsg.serialize(sipmsg)
+      { :ok, Map.put(initial_state, :msgstr, msgstr) }
+    rescue
+      e in RuntimeError ->
+        Logger.debug([ transid: sipmsg.transid, message: e.message])
+        { :invalid_sip_msg, initial_state }
+    end
+  end
+
+
+
+  # Callbacks
+
+  @impl true
+  def init({ t_mod, t_pid, sipmsg, app_pid, ring_timeout }) do
+    initial_state = %{ msg: sipmsg, tmod: t_mod, tpid: t_pid, app: app_pid, t2: ring_timeout,
+                       t_isreliable: apply(t_mod, :is_reliable, []),  state: :sending }
+
+    case safe_serialize(initial_state, sipmsg) do
+      {:ok, initial_state} ->
+        case GenServer.call(t_pid, :sendmsg, initial_state.msgstr ) do
+          :ok ->
+            if not initial_state.t_isreliable do
+              schedule_timer_T1(initial_state)
+            end
+            { :ok, initial_state }
+
+          code ->
+            Logger.error([ transid: sipmsg.transid, message: "ICT: Fail to send message: #{code}"])
+            { :stop, initial_state }
+        end
+
+      { code, initial_state} ->
+        Logger.error([ transid: sipmsg.transid, message: "ICT: Fail to serialize SIP message #{code}"])
+        { :stop, initial_state }
+    end
+
+  end
+
+  @impl true
+  # CANCEL  current transaction from dialog layer
+  def handle_call(:cancel, _from, state) do
+    if state.state in [ :sending, :proceeding ] do
+      cancel = state.sipmsg |> SIPMsgOps.cancel_request() |> SIPMsg.serialize()
+      Logger.info([ transid: state.sipmsg.transid, message: "ICT: sending CANCEL"])
+      case GenServer.call(state.tpid, { :sendmsg, cancel }  ) do
+        :ok ->
+          { :reply, :ok, %{ state | state: :cancelling }}
+
+        code ->
+          Logger.error([ transid: state.sipmsg.transid, message: "ICT: Fail to send CANCEL message #{code}"])
+          { :reply, :transport_error, state}
+      end
+    else
+      Logger.debug([ transid: state.sipmsg.transid, message: "Cannot CANCEL transaction in #{state.state} state"])
+      { :reply, :bad_state, state}
+    end
+  end
+
+  # ACK the transaction (only needed in case of 200 OK received)
+  def handle_call(:ack, _from, state) do
+    if state.state == :confirmed do
+      routeset = case Map.fetch(state, :route) do
+        {:ok, routeset} -> routeset
+        :error -> nil
+      end
+
+      remote_contact = case Map.fetch(state, :remotecontact) do
+        {:ok, rcontact} -> rcontact
+        :error -> nil
+      end
+      ack_sent = state.sipmsg |> SIPMsgOps.ack_request(remote_contact, routeset) |> SIPMsg.serialize()
+      Logger.info([ transid: state.sipmsg.transid, message: "Sending ACK"])
+      case GenServer.call(state.tpid, {:sendmsg, ack_sent} ) do
+        :ok ->
+          if state.t_isreliable do
+            Logger.debug([ transid: state.sipmsg.transid, message: "ACK sent: #{state.state} -> terminated"])
+            schedule_timer_K(state, 0)
+            { :reply, :ok, %{ state | ack: ack_sent, state: :terminated }}
+          else
+            # RFC 3261 clause 17.1.2.2 arm timer K for unreliable transport
+            Logger.debug([ transid: state.sipmsg.transid, message: "ACK sent. Arming timer_K"])
+            state = %{ schedule_timer_K(state, 5000) | ack: ack_sent }
+            { :reply, :ok, state }
+          end
+        code ->
+          Logger.error([ transid: state.sipmsg.transid, message: "ICT: Fail to send ACK message #{code}"])
+          # Arm a timer to destroy the transaction
+          { :reply, :transport_error, state}
+      end
+    end
+  end
+
+  # Handle privisional (1xx) responses
+  defp handle_sip_response(state, sipmsg) when SIPMsgOps.is_1xx_resp(sipmsg) do
+    Logger.debug([ transid: sipmsg.transid, message: "Received prov resp #{sipmsg.response_code}"])
+    cond do
+      state.state == :sending ->
+        # Send provisional response to app layer
+        # Todo: support reliabilty and PRACK
+        send(sipmsg.app, { :response, sipmsg })
+        Logger.debug([ transid: sipmsg.transid, message: "state: sending -> proceeding"])
+        %{ sipmsg | state: :proceeding }
+
+      state.state == :proceeding ->
+        send(sipmsg.app, { :response, sipmsg })
+        state
+
+      true ->
+        Logger.debug([ transid: sipmsg.transid, message: "state: #{state.state}. Ignoring resp."])
+        state
+    end
+  end
+
+  # Handle OK responses (2xx)
+  defp handle_sip_response(state, sip_resp) when SIPMsgOps.is_2xx_resp(sip_resp) do
+    Logger.debug([ transid: sip_resp.transid, message: "Received #{sip_resp.response_code} final resp"])
+    cond do
+      state.state in [ :sending, :proceeding ] ->
+        send(state.app, { :response, sip_resp })
+        Logger.debug([ transid: sip_resp.transid, message: "state: #{state.state} -> confirmed"])
+        routeset = case Map.fetch(sip_resp, :route) do
+          { :ok, routeset } -> routeset
+          :error -> nil
+        end
+        %{ state | state: :confirmed, remotecontact: sip_resp.contact, route: routeset }
+
+      # Corner case when 200 OK retransmission is still not acked by app layer.
+      state.state == :confirmed ->
+        send(state.app, { :response, sip_resp })
+        state
+
+      # Handle 200 OK retransmission on unrelable transport (UDP)
+      state.state == :terminated and is_bitstring(state.ack) ->
+        GenServer.call(state.tpid, :sendmsg, state.ack )
+        state
+
+      true ->
+        Logger.debug([ transid: sip_resp.transid, message: "state: #{state.state}. Ignoring resp."])
+        state
+    end
+  end
+
+  # Handle 4xx, 5xx, 6xx responses
+  defp handle_sip_response(state, sipmsg) when SIPMsgOps.is_failure_resp(sipmsg) do
+    Logger.debug([ transid: sipmsg.transid, message: "Received #{sipmsg.response_code} failure resp"])
+    cond do
+      state.state in [ :sending, :proceeding ] ->
+        # Send the message to the application layer
+        send(sipmsg.app, { :response, sipmsg })
+        # Send ACK automatically
+        { :reply, _reply, new_state } = handle_call(:ack, self(), state)
+        new_state
+
+        state.state == :confirmed  and is_bitstring(state.ack) ->
+          #Resend the same ack message
+          GenServer.call(state.tpid, :sendmsg, state.ack )
+          state
+
+        true ->
+          Logger.debug([ transid: sipmsg.transid, message: "state: #{state.state}. Ignoring resp."])
+          state
+      end
+  end
+
+  defp handle_cancel_response(state, siprsp) do
+    if siprsp.response_code == 200 do
+      state
+    else
+      send(state.app, { :cancel_rejected, siprsp.response_code})
+      state
+    end
+  end
+  @impl true
+   # Process SIP response from transport layer
+  def handle_cast({ :onsipmsg, siprsp }, state) do
+    cond do
+      is_atom(siprsp.method) ->
+        Logger.warning([ transid: state.sipmsg.transid, message: "Received an #{siprsp.method} SIP request. But this is a client transaction'"])
+        {:noreply, state}
+
+      # The response matches the INVITE req
+      state.sipmsg.cseq == siprsp.cseq ->
+        new_state = handle_sip_response(state, siprsp)
+        {:noreply, new_state}
+
+
+      # The response matches the CANCEL req
+      siprsp.cseq == { hd(state.sipmsg.cseq), :CANCEL } ->
+        new_state = handle_cancel_response(state, siprsp)
+        {:noreply, new_state}
+
+      true ->
+        Logger.warning([ transid: state.sipmsg.transid, message: "Response CSeq #{siprsp.cseq} does not match transaction requests'"])
+        {:noreply, state}
+    end
+  end
+
+  @impl true
+  # Handle T1 time retransmission
+  def handle_info({ :timerT1, ms }, state) do
+    handle_timer({ :timerT1, ms }, state)
+  end
+end
---
+++ lib/SIPTransactTimer.ex
@@ -0,0 +1,89 @@
+defmodule SIP.Trans.Timer do
+  require Logger
+  @timer_T1_val 32000
+  @timer_T2_val 4000
+
+  # Arm T1 timer
+  def schedule_timer_T1(state, ms \\ 500) do
+    Process.send_after(self(), { :timerT1, ms }, ms )
+    state
+  end
+
+  def schedule_timer_T2(state, ms \\ @timer_T2_val) do
+    schedule_generic_timer(state, :timerT2, :t2, ms)
+  end
+
+  @doc """
+  Schedule a generic cancellable timer
+  if ms is nil, cancel timer
+  if ms is 0, fire timer immediatly
+  if ms > 0, schedule timer
+  """
+  @spec schedule_generic_timer(state :: map() , timer_id :: atom() , timer_field :: atom(), ms :: integer() ) :: map()
+  def schedule_generic_timer(state, timer_id, timer_field, ms) when is_atom(timer_id) do
+    # If needed cancel previous timer
+    state = case Map.fetch(state, timer_field) do
+      { :ok, nil } -> state
+      { :ok, timer_ref } -> :erlang.cancel_timer(timer_ref)
+      :error -> Map.put(state, timer_field, nil)
+    end
+
+    case ms do
+      nil -> state # Nothing to do as timer is already cancelled
+
+      # Send message immediatly
+      0 ->
+        send(self(), timer_id)
+        state
+
+      # Schedule timer
+      millis when millis > 0 ->
+        tref = :erlang.start_timer(millis, self(), timer_id)
+        Map.put(state, timer_field, tref)
+    end
+  end
+
+  @doc "Schedule/reschedule the K timer and save its reference (pid) in the transaction state"
+  def schedule_timer_K(state, ms) do
+    schedule_generic_timer(state, :timerK, :timerk, ms)
+  end
+
+  @doc "Handle timer messages"
+  def handle_timer({ :timerT1, ms }, state) when ms < @timer_T1_val and state.state == :sending do
+    if not state.t_isreliable do
+      # If transport is not reliable, retransmit
+      code = GenServer.call(state.tpid, { :sendmsg, state.msgstr } )
+      if code != :ok do
+        Logger.error([ transid: state.sipmsg.transid, message: "timer_T1: Fail to retransmit message: #{code}"])
+      end
+    end
+    schedule_timer_T1(state, ms*2)
+    { :noreply, state }
+  end
+
+  def handle_timer({ :timerT1, ms }, state) when ms >= @timer_T1_val and state.state == :sending do
+    Logger.error([ transid: state.sipmsg.transid, message: "timer_T1: transaction timed out (retrans)."])
+    send(state.sipmsg.app, {:timeout, :timer_T1})
+    { :stop, state, "timer_T1: transaction timed out (retrans)." }
+  end
+
+  def handle_timer({ :timerT1, _ms }, state) when state.state != :sending do
+    { :noreply, state }
+  end
+
+  def handle_timer( :timerT2, state) when state.state == :proceeding do
+    send(state.sipmsg.app, {:timeout, :timer_T2})
+    Logger.error([ transid: state.sipmsg.transid, message: "timer_T2: no final response receveived on time."])
+    { :stop, state, "SIP transaction timeout (timer T2)" }
+  end
+
+  def handle_timer( :timerK, state) when state.state in [ :confirmed, :terminated ] do
+    # Timer K expired: destroy transaction
+    Logger.debug([ transid: state.sipmsg.transid, message: "timer_K: SIP transaction terminated."])
+    { :stop, state, "SIP transactipn timeout (timer K)" }
+  end
+
+  def handle_timer( :timer_K, state) do
+    { :noreply, state }
+  end
+end
