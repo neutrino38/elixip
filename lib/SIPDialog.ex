4e1be36dc3a67ca5f15dde80bf05ae548ea5a12a
Emmanuel Buu <emmanuel.buu@ives.fr>
2024-09-23 18:46:04 +0200

    Dialog implementation

--- lib/SIPDialog.ex
+++ lib/SIPDialog.ex
@@ -3,6 +3,7 @@ defmodule SIP.Dialog do
   require Logger
   require Registry
   require SIP.Uri
+  use GenServer
 
 
   defstruct [
@@ -15,9 +16,23 @@ defmodule SIP.Dialog do
     app: nil, # PID of the application
     state: :inital,
     debuglog: true, # If we should output debug logs for this dialog
-    expirationtimer: nil
+    expirationtimer: nil,
+    cseq: 1
   ]
 
+
+  defmodule Listener do
+    use Agent
+
+    defstruct [
+      app: nil,
+      callhandlingmodule: nil,
+      onnewregistration: nil,
+    ]
+    def start() do
+
+    end
+  end
   @spec start() :: :error | :ok
   @doc "Start the dialog layer"
   def start() do
@@ -33,7 +48,38 @@ defmodule SIP.Dialog do
     end
   end
 
+  # -------- GenServer callbacks --------------------
 
+
+  @impl true
+  def init({ req, direction, pid, timeout, debug }) do
+    if direction == :inbound do
+      state = %SIP.Dialog{ msg: req, direction: direction, app: nil, expirationtimer: timeout, debuglog: debug,
+                   transactions: [ pid ] }
+
+      { :ok, state }
+    else
+      state = %SIP.Dialog{ msg: req, direction: direction, app: pid, expirationtimer: timeout, debuglog: debug,
+                   transactions: [] }
+
+      #In case of an outbound dialog, start a, UAC transaction
+      case SIP.Transac.start_uac_transaction( req, timeout ) do
+        { :ok, transaction_pid } -> { :ok, %SIP.Dialog{ state | transactions: [ transaction_pid ] } }
+        { code, _extra } -> { :stop, code }
+      end
+    end
+  end
+
+
+  @impl true
+  def handle_call({ :setapppid, app_pid }, _from, state) do
+    if state.direction == :inbound and state.app == nil do
+      { :reply, :ok,  %SIP.Dialog{ state | app: app_pid } }
+    else
+      { :reply, :alreadybound, state }
+    end
+  end
+  # ---------------------- API ----------------------
   # Obtain the triplet that uniquely identify a dialog
   defp get_dialog_id(req) do
     { _code, fromtag } = SIP.Uri.get_uri_param(req.from, "tag")
@@ -65,7 +111,7 @@ defmodule SIP.Dialog do
   end
 
   @doc "Start a dialog"
-  def start_dialog(req, timeout, direction \\ :outbound, debug \\ false) do
+  def start_dialog(req, timeout, direction \\ :outbound, debug \\ false) when is_atom(req.method) do
     # Obtain create the dialog id { fromtag, callid, totag } that identify the SIP dialog according to RFC 3261
     # Using the recusion and pattern matching
     { req2, dialog_id } = get_or_create_dialog_id(req)
@@ -89,13 +135,47 @@ defmodule SIP.Dialog do
   end
 
 
-  def process_incoming_request(req, transact_pid) when is_map(req) and is_atom(req.method) do
+  def process_incoming_request(req, transact_pid, debug) when is_map(req) and is_atom(req.method) do
     { req2, dialog_id } = get_or_create_dialog_id(req)
     case Registry.lookup(Registry.Dialog, dialog_id) do
-      # No such transction
+      # No such dialog - create it if the request
       [] ->
-        SIP.Transac.reply(transact_pid, 481, " Call/Transaction Does Not Exist")
-        { :no_matching_transaction, req2 }
+        case req.method do
+          :INVITE ->
+            # todo, add a timeout global parameter
+            start_dialog(req2, 1800, :inbound, debug)
+
+          :OPTIONS ->
+            start_dialog(req2, 60, :inbound, false)
+
+          :MESSAGE ->
+            start_dialog(req2, 60, :inbound, debug)
+
+          :ACK ->
+            # to add error log - ACK should be in dialog
+            :no_matching_dialog
+
+          :PRACK ->
+            # to add error log - ACK should be in dialog
+            :no_matching_dialog
+
+          m when m in [ :PUBLISH, :REGISTER, :SUBSCRIBE ] ->
+            #Todo compulte timeout from refresh contact period
+            to = 600
+            start_dialog(req2, to, :inbound, debug)
+
+          :UPDATE ->
+            SIP.Transac.reply(transact_pid, 481, " Call/Transaction Does Not Exist")
+            :no_matching_dialog
+
+          :BYE ->
+            SIP.Transac.reply(transact_pid, 481, " Call/Transaction Does Not Exist")
+            :no_matching_dialog
+
+          _ ->
+            SIP.Transac.reply(transact_pid, 500, " Unsupported request")
+            :no_matching_dialog
+        end
 
       # Found a matching transaction. Dispatch the SIP msg to it
       # We do not use dispatch because we have already looked up the transaction list
@@ -105,6 +185,5 @@ defmodule SIP.Dialog do
         GenServer.cast(pid, {:onsipmsg, req2, transact_pid})
         { pid, req2 }
     end
-
   end
 end
