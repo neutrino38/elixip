a43bc740f5659697921a9352c080e7f61471c536
Emmanuel Buu <emmanuel.buu@ives.fr>
2024-10-07 23:02:46 +0200

    start dialog register test implementation

--- test/sip_register_test.exs
+++ test/sip_register_test.exs
@@ -3,6 +3,35 @@ defmodule SIP.Test.Register do
   require SIP.Dialog
   doctest SIP.Dialog
 
+  defmodule Registrar do
+    use SIP.Session.Registrar
+
+    defp registrar_process_loop(state) do
+      receive do
+        { :REGISTER, reg } ->
+          registrar_process_loop(state)
+
+        :stop -> nil
+      end
+    end
+
+
+    @impl true
+    def on_new_registration(dialog_id, register) do
+      case Process.whereis(:test_registrar) do
+        nil ->
+          state = %{ dialogid: dialog_id }
+          new_reg_pid = spawn_link(fn state -> registrar_process_loop(state) end)
+          Process.register(new_reg_pid, :test_registrar)
+          { :accept, registrar_pid }
+
+        registrar_pid when is_pid(registrar_pid) ->
+          { :accept, registrar_pid }
+      end
+    end
+
+  end
+
   setup_all do
     # Initialize transaction and transport layers
     :ok = SIP.Transac.start()
